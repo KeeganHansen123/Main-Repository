17998a64963f29eb3505d2e137b8f07f28ea04b7
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-10-06 09:11:23 -0500

    AHWEIORsdkljv

---
+++ Final Project/src/FinalApplication/Game.java
@@ -0,0 +1,18 @@
+package FinalApplication;
+
+import javax.swing.JOptionPane;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gameStart();
+		
+		
+	}
+		public static void gameStart() {
+			
+		JOptionPane.showMessageDialog(null,  "You did it");
+	}
+
+}
--- Final Project/src/index/ButtonTesting.java
+++ Final Project/src/index/ButtonTesting.java
@@ -1,6 +1,9 @@
 package index;
 
 import javax.swing.*;
+import javax.swing.JOptionPane;
+import FinalApplication.Game;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -10,6 +13,13 @@ public class ButtonTesting extends JFrame {
 		private final int SIZE = 180;
 		private Container con = getContentPane();
 		private JButton button = new JButton("Play");
+		
+		
+		private final int SIZE1 = 180;
+		private Container con1 = getContentPane();
+		private JButton button1 = new JButton("Quit");
+		
+		
 		public ButtonTesting()
 		{
 			setSize(SIZE, SIZE);
@@ -18,15 +28,31 @@ public class ButtonTesting extends JFrame {
 			con.setBackground(Color.BLACK);
 			button.setBackground(Color.BLUE);
 			button.setForeground(Color.WHITE);
+			button.setMnemonic(KeyEvent.VK_M);
+			button.setActionCommand("go");
+			button.addActionListener(m -> Game.gameStart());
+			
+			setSize(SIZE1, SIZE1);
+			con1.setLayout(new FlowLayout());
+			con1.add(button1);
+			con1.setBackground(Color.BLACK);
+			button1.setBackground(Color.BLUE);
+			button1.setForeground(Color.WHITE);
+			button1.setMnemonic(KeyEvent.VK_E);
+			button1.setActionCommand("stop");
+			button1.addActionListener(e -> System.exit(0));
 		}
 		public static void main(String[] args)
 		{
 			ButtonTesting frame = new ButtonTesting();
 			frame.setVisible(true);
+		
+			}
+		public void actionPerformed(ActionEvent M) {			}
 		}
 
 			
-		}
+		
 	
 
 
