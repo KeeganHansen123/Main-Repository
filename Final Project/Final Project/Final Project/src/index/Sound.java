f645b1dfe4c5e14ba3c4840519e55178ece8360c
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-10-06 08:52:14 -0500

    update

---
+++ Final Project/Final Project/Final Project/src/index/Sound.java
@@ -0,0 +1,22 @@
+package index;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
+	public class Sound {
+
+		public static void main(String[] args) throws Exception 
+		{
+			URL url = Sound.class.getResource("");
+			AudioClip clip = Applet.newAudioClip(url);
+			AudioClip clip2 = Applet.newAudioClip(url);
+			clip.play();
+			Thread.sleep(1000);
+			clip2.loop();
+			Thread.sleep(20000);
+			clip2.stop();
+			
+		}
+		
+}
