67e480aa4768f09d11eb7667b7c7c947537a7b91
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-10-13 09:01:03 -0500

    update

--- Final Project/src/index/ManyTries.java
+++ Final Project/src/index/ManyTries.java
@@ -6,26 +6,22 @@ public class ManyTries {
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		 int attemptsNum = 0;
-		    final int maxAttempts = 3;
-		    int user;
-			int comp;
-			do {
-		        System.out.print("Guess a number between 1 and 10: ");
-		        Scanner input = new Scanner(System.in);
-				user = input.nextInt();
-
-		        if (user > comp)
-		            System.out.println("My number is less than " + user + ".");
-		        else if (user < comp)
-		            System.out.println("My number is greater than " + user + ".");
-		        else
-		            System.out.println("Correct! " + comp + " was my number! ");
-		    } while (user != comp && ++attemptsNum <maxAttempts );
-
-		    if (attemptsNum == maxAttempts)
-		        System.out.println("You loose. The number was: " + comp);
+		final String PASSWORD = "Test";
+		
+		Scanner input = new Scanner(System.in);
+		int tries = 3;
+		String password = "";
+		while (tries -- > 0 && !PASSWORD.equals(password))
+		{
+			System.out.println("Enter your password: ");
+			password = input.nextLine();
+			if(password.equals(PASSWORD))
+				System.out.println("Welcome");
+			else
+				System.out.println("Incorrect Attempts remaining: " + tries);
+		}
+			
+				
 
 	}
 
